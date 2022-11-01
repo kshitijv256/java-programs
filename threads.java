19fcc54f618ff2d15cccdd1db9f236ddeb3bd6fd
Kshitij Verma <kshitijv250@gmail.com>
2022-11-01 14:24:52 +0530

    initial commit

---
+++ threads.java
@@ -0,0 +1,17 @@
+
+
+class threads extends Thread {
+    public static void main(String[] args) {
+        threads t = new threads();
+        t.start();
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("outside the thread");
+        }
+
+    }
+    public void run() {
+        for (int i = 0; i < 1000; i++) {
+            System.out.println("In the thread");
+        }
+    }
+}
