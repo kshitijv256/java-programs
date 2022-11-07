f951a2faa7b4ec603d977e852806594401745d1b
Kshitij Verma <101321276+kshitijv256@users.noreply.github.com>
2022-11-07 23:38:50 +0530

    code : ExceptionPropogation.java

---
+++ ExceptionPropagation.java
@@ -0,0 +1,19 @@
+class ExceptionPropagation{  
+  void m(){  
+    int data=50/0;  
+  }  
+  void n(){  
+    m();  
+  }  
+  void p(){  
+   try{  
+    n();  
+   }catch(Exception e){System.out.println("exception caught and handled\n");
+   }
+  }  
+  public static void main(String args[]){  
+   ExceptionPropagation obj=new ExceptionPropagation();  
+   obj.p();  
+   System.out.println("continuing flow");
+  }  
+}  
