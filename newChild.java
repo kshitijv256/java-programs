7860983359ada1b9b8e7c842863b817cc37aa9d3
Kshitij Verma <kshitijv250@gmail.com>
2022-11-19 21:51:21 +0530

    code: newChild.java

---
+++ newChild.java
@@ -0,0 +1,15 @@
+class newParent {
+    void method() {
+        System.out.println("Parent's method()");
+    }
+}
+public class newChild extends newParent {
+    public void method() {
+        System.out.println("Child's method()");
+        super.method();
+    }
+    public static void main(String[] args) {
+        newChild obj = new newChild();
+        obj.method();
+    }    
+}
