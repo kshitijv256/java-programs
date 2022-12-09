4a862a5177b9283697597e4975b5a5d6817686f8
Kshitij Verma <kshitijv250@gmail.com>
2022-12-09 23:11:27 +0530

    Code: Interfaces in java Example

---
+++ interfaces/AuxClass.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+public class AuxClass extends HiddenClass implements MyInterface{
+    public void printer(){
+        System.out.println("working implementation");
+    }
+
+    @Override
+    void myMethod() {
+        System.out.println("My Hidden Method");
+    }
+}
---
+++ interfaces/HiddenClass.java
@@ -0,0 +1,5 @@
+package interfaces;
+
+public abstract class HiddenClass {
+    abstract void myMethod();
+}
